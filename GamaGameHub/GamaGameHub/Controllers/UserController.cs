c53f70bee7397ba4ed6ea5815a6b4f72e97564ae
konstantin.filosofov@gmail.com <konstantin.filosofov@gmail.com>
2023-11-02 09:34:23 +0200

    added login register pagfes

---
+++ GamaGameHub/GamaGameHub/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamaGameHub.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
