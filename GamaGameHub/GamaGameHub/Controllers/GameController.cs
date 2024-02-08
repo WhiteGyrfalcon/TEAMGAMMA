dea9f5c4c354ed5f9fbe39e47cf755582afc8bb8
marielabelcheva <meribelcheva@gmail.com>
2024-02-08 13:05:39 +0200

    fixed

--- GamaGameHub/GamaGameHub/Controllers/GameController.cs
+++ GamaGameHub/GamaGameHub/Controllers/GameController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GamaGameHub.Controllers
 {
-    
+    [Authorize]
     public class GameController : Controller
     {
         private readonly IGameService gameService;
